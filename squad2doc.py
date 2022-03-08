2e8177931dad575d38ae905641892009087f1ef0
Filip Ginter <filip.ginter@gmail.com>
2022-03-08 13:53:29 +0200

    meta

--- squad2doc.py
+++ squad2doc.py
@@ -3,6 +3,7 @@ import sys
 from pprint import pprint
 import seaborn
 import docx
+import tqdm
 
 def make_spans(color_map,characters):
     result=[]
@@ -32,7 +33,7 @@ def index_fonts():
             if not line.startswith("#"):
                 continue
             palette.append(line)
-    print(palette)
+    #print(palette)
     assert len(palette)==len(set(palette))
 
     rgb_colors=[]
@@ -41,7 +42,9 @@ def index_fonts():
         rgb_colors.append(color)
 
     return rgb_colors
-        
+
+rgb_colors=index_fonts()
+
 def para2txt(p,p_idx,doc):
     total_len=0
     
@@ -54,6 +57,7 @@ def para2txt(p,p_idx,doc):
     
     ctx=p["context"]
     color_map=[[] for _ in range(len(ctx))]
+
     for qa in p["qas"]:
         q=qa["question"]
         q_id=qa["id"]
@@ -68,13 +72,13 @@ def para2txt(p,p_idx,doc):
             #print()
 
     #make a lookup table such that each unique overlap of answer ids has a color of its own whew
-    color_lookup={}
+    color_lookup={"":0}
     for answer_list in color_map: #this is a list of all questions which overlap as "questionid_answeridx" strings
         answer_list="+".join(answer_list) #make it a single string
         color_lookup.setdefault(answer_list,len(color_lookup))
 
-    print(color_lookup)
-    colors=index_fonts()
+    #print(color_lookup)
+    #colors=index_fonts()
 
     
     spans=make_spans(color_map,ctx)
@@ -82,12 +86,14 @@ def para2txt(p,p_idx,doc):
         r=pgraph.add_run(txt)
         font_idx=color_lookup.get("+".join(span_list),None)
         if font_idx is not None:
-            r.font.color.rgb=colors[font_idx]
+            r.font.color.rgb=rgb_colors[font_idx]
 
     total_len+=len(ctx)
-            
+
+    question_list=[]
     for q_idx,qa in enumerate(p["qas"]):
         q=qa["question"]
+        question_list.append(qa["id"])
         pgraph=doc.add_paragraph("")
         t=f"Question {q_idx}"
         total_len+=len(t)
@@ -95,7 +101,7 @@ def para2txt(p,p_idx,doc):
         pgraph=doc.add_paragraph(q)
         total_len+=len(q)
 
-    return total_len
+    return total_len, color_lookup, question_list
 
 if __name__=="__main__":
 
@@ -106,27 +112,32 @@ if __name__=="__main__":
         
     sizes=[]
     d_idx=0
-    for fname in sys.argv[1:]:
-        with open(fname,"rt") as f:
-            data=json.load(f)["data"]
-            for d in data:
-                title=d["title"]
-                pgraph=doc.add_paragraph("")
-                r=pgraph.add_run(f"Document number {d_idx}")
-                r.bold=True
-                r.underline=True
-                ps=d["paragraphs"]
-                for p_idx, p in enumerate(ps):
-                    total_len+=para2txt(p,p_idx,doc)
-                doc.add_page_break()
-                d_idx+=1
-                if total_len>950000: #document full!
-                    doc.save(f"squad2-en/squad2_{file_counter:03d}.docx")
-                    file_counter+=1
-                    total_len=0
-                    doc=docx.Document()
-    else:
-        doc.save(f"squad2-en/squad2_{file_counter:03d}.docx")
+    with open("squad2-en/meta.jsonl","wt") as meta:
+        for fname in sys.argv[1:]:
+            with open(fname,"rt") as f:
+                data=json.load(f)["data"]
+                for d in tqdm.tqdm(data):
+                    docmeta={"file":fname,"title":d["title"],"sequence_idx":d_idx,"paragraphs":[]}
+                    title=d["title"]
+                    pgraph=doc.add_paragraph("")
+                    r=pgraph.add_run(f"Document number {d_idx}")
+                    r.bold=True
+                    r.underline=True
+                    ps=d["paragraphs"]
+                    for p_idx, p in enumerate(ps):
+                        l,color_map,question_list=para2txt(p,p_idx,doc)
+                        docmeta["paragraphs"].append((p_idx,color_map,question_list))
+                        total_len+=l
+                    print(json.dumps(docmeta,sort_keys=True,ensure_ascii=False),file=meta,flush=True)
+                    doc.add_page_break()
+                    d_idx+=1
+                    if total_len>950000: #document full!
+                        doc.save(f"squad2-en/squad2_{file_counter:03d}.docx")
+                        file_counter+=1
+                        total_len=0
+                        doc=docx.Document()
+        else:
+            doc.save(f"squad2-en/squad2_{file_counter:03d}.docx")
                     
                     
 
