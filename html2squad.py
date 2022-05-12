2f94c4f9b82f31233364d68c30fd47b877831ad6
Emil Nuutinen <emil@emilnuutinen.com>
2022-05-12 16:17:07 +0300

    Parse all the required information from the html files (needs testing)

--- html2squad.py
+++ html2squad.py
@@ -1,6 +1,5 @@
 import bs4
 from bs4 import BeautifulSoup
-import re
 
 # colors from palette.txt for easier access
 colors = ['#696969','#a9a9a9','#dcdcdc','#2f4f4f','#556b2f','#6b8e23','#a0522d','#228b22','#191970','#8b0000','#483d8b','#3cb371','#bc8f8f','#663399','#008080','#bdb76b','#4682b4','#d2691e','#9acd32','#cd5c5c','#00008b','#32cd32','#daa520','#7f007f','#8fbc8f','#b03060','#66cdaa','#9932cc','#ff4500','#00ced1','#ff8c00','#ffd700','#c71585','#0000cd','#deb887','#00ff00','#00ff7f','#4169e1','#e9967a','#dc143c','#00ffff','#00bfff','#f4a460','#9370db','#0000ff','#a020f0','#adff2f','#ff6347','#da70d6','#d8bfd8','#ff00ff','#db7093','#f0e68c','#ffff54','#6495ed','#dda0dd','#90ee90','#87ceeb','#ff1493','#afeeee','#7fffd4','#ff69b4','#ffe4c4','#ffb6c1']
@@ -72,14 +71,15 @@ with open('squad2-fi-raw/html/squad2_000 fi.html', 'r') as file:
                 answer_pos = get_ans_pos(para, ans_colors)
             #print(para_text)
             print("Colors: ",ans_colors)
-            print("Colors: ",color_ids)
+            print("Color ID's: ",color_ids)
             print("Positions: ",answer_pos)
             print("Answers: ",answers)
             continue
         
         # Get questions
         if is_b(elem) and "Kysymys" in elem.get_text():
+            question_id = ''.join([i for i in elem.get_text().split() if i.isdigit()])
             question = elem.find_next("p").get_text().replace("\n", " ")
-            print(question)
+            print(question_id,question)
             continue
 
