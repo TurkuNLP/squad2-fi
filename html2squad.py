6f27bfe4c8b47454ac7cb81118f56016a96829f1
Emil Nuutinen <emil@emilnuutinen.com>
2022-05-16 13:42:05 +0300

    Add starting index only for the first part of the answer

--- html2squad.py
+++ html2squad.py
@@ -37,10 +37,13 @@ def get_ans_pos(para, colors):
     color_start = 13 # Length from the color to the start of the font tag
     p_tag = 33 # length of the p tag in the start of the string
     tags_len = color_start + p_tag
+    seen = []
     for i,color in enumerate(colors):
         # Get the positions of the answers in plain text paragraphs
-        index = para.find(color)-font_tag*i-tags_len
-        positions.append(index)
+        if color not in seen:
+            index = para.find(color)-font_tag*i-tags_len
+            positions.append(index)
+        seen.append(color)
     return positions
 
 with open('squad2-fi-raw/html/squad2_000 fi.html', 'r') as file:
